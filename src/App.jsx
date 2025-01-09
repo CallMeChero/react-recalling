4e1dc986b9478bef7e871a1785da41cd7b269541
CallMeChero <david.ceranic@gmail.com>
2025-01-09 21:28:27 +0100

    Handling form submission

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 function App() {
 
-  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
 
   function showModalHandler() {
     setModalIsVisible(true)
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,31 +1,18 @@
 import Post from './Post';
 import NewPost from './NewPost';
 import classes from './PostList.module.css';
-import { useState } from 'react';
 import Modal from './Modal';
 
 function PostList({onHideModal, modalIsVisible}) {
-
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value)
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value)
-    }
 
     return (
         <>
             { modalIsVisible 
                 && <Modal onClose={onHideModal}>
-                    <NewPost onAuthorChange={authorChangeHandler} onBodyChange={bodyChangeHandler}/>
+                    <NewPost onCancel={onHideModal}/>
                 </Modal>
             }
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Petra" body="React is very awesome"/>
             </ul>
         </>
