8e838894b49e72b7abeb6febe3d684d925cea508
CallMeChero <david.ceranic@gmail.com>
2025-01-09 15:03:54 +0100

    Lifting state up to parent and share it to other component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,26 @@
 import Post from './Post';
 import NewPost from './NewPost';
 import classes from './PostList.module.css';
+import { useState } from 'react';
 
 function PostList() {
+
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value)
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value)
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onAuthorChange={authorChangeHandler} onBodyChange={bodyChangeHandler}/>
             <ul className={classes.posts}>
-                <Post author="David" body="React is awesome"/>
+                <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Petra" body="React is very awesome"/>
             </ul>
         </>
