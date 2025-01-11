10f1c96be4ddfcc43c6482c0a3115609977cbf69
CallMeChero <david.ceranic@gmail.com>
2025-01-11 15:06:35 +0100

    Handling load post state

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,13 +2,47 @@ import Post from './Post';
 import NewPost from './NewPost';
 import classes from './PostList.module.css';
 import Modal from './Modal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function PostList({onHideModal, modalIsVisible}) {
 
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(undefined);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                setIsFetching(true);
+                setErrorMessage(undefined)
+                const response = await fetch('http://localhost:8080/posts');
+                console.log(response)
+                const resData = await response.json();
+                if(!response.ok) {
+                    setErrorMessage('An error has occured. Please contact administrator');
+                }
+                setPosts(resData.posts ?? []);
+                setIsFetching(false);
+            } catch(err) {
+                setErrorMessage('An error has occured. Please contact administrator');
+                setIsFetching(false);
+            }
+            
+        }
+
+        fetchPosts();
+    }, [
+        // dependency kada se okine useEffect, jer da je vani fetch bi napravio infite loop npr
+    ])
 
     function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
         // nije dobra praksa jer snapshot moze biti
         // setPosts([postData, ...posts])
         setPosts((existingPosts) => [postData, ...existingPosts])
@@ -22,15 +56,27 @@ function PostList({onHideModal, modalIsVisible}) {
                     <NewPost onCancel={onHideModal} onAddPost={addPostHandler}/>
                 </Modal>
             }
-            <ul className={posts.length ? classes.posts : null}>
-                { posts.length
-                    ? posts.map((post) => <Post key={post.author} author={post.author} body={post.body} />)
-                    : <div style={{ textAlign: 'center', color: 'white' }}>
-                        <h2>There are no posts yet.</h2>
-                        <p>Start adding some!</p>
-                    </div>
-                }
-            </ul>
+            { !isFetching && posts.length && (
+                <ul className={classes.posts}>
+                    { posts.map((post) => <Post key={post.author} author={post.author} body={post.body} />) }
+                </ul>
+            )}
+            {  !errorMessage && !isFetching && posts.length === 0 && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <h2>There are no posts yet.</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
+            { isFetching && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <p>Loading posts...</p>
+                </div>
+            )}
+            { errorMessage && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <p>{ errorMessage }</p>
+                </div>
+            )}
         </>
     );
 }
