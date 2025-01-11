e31bce5c9a919d863372642b5ec371abd733a44a
CallMeChero <david.ceranic@gmail.com>
2025-01-11 15:44:32 +0100

    Recall of router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import NewPost from './components/NewPost'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './routes/RootLayout'
+
+const router = createBrowserRouter([
+  { path: '/', element: <RootLayout />, children: [
+    { path: '/', element: <App /> },
+    { path: '/create-post', element: <NewPost />}
+  ]}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // React Strict Mode da imamo checkove za potencijalne probleme, legacy code itd
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
